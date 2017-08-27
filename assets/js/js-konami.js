3276373230c4f29cd2a0583c7a2d6e6de6d63ce8
Mike Yamato <Yamato@Yamatos-MacBook-Pro-Retina.local>
2017-08-26 17:39:03 -0700

    made responsive

---
+++ assets/js/js-konami.js
@@ -0,0 +1,17 @@
+function onKonamiCode(cb) {
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    console.log(input, key.indexOf(input));
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+onKonamiCode(function () {
+  alert('\Congrats. You now have access to the super secret page.')
+})
